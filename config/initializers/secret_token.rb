18a020c269ea0facb7fff823b268dc234991ae20
pochi <pochi.black@gmail.com>
2011-05-27 12:32:48 +0900

    Create sample notification system

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Notification::Application.config.secret_token = 'd0b5439095bebf8dd27777bc9b9bc1447ece41ff5c7b7054e93f84f2be75d4e036409d994983cda22efb87f68915e460917d5645b1e271f4c1344f767aa0223e'
